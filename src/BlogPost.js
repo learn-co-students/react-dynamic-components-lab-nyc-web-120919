18306707e295dc7b857ea131a08de8770e7af9e4
Candillan <calvin.baker128@gmail.com>
2020-02-18 16:42:56 -0500

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Comment from './Comment.js';
 // are we missing an import?
 
 export default class BlogPost extends Component {
@@ -9,6 +10,8 @@ export default class BlogPost extends Component {
       "I am no longer accepting the things I cannot change. I am changing the things I cannot accept. - Angela Davis",
       "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway. It's easy to tell when a question is coming from a good place. Then listen some more. Sometimes people just want to feel heard. Here's to possibilities of friendship and connection and understanding. - Chimamanda Ngozi Adichie"
     ]
+
+
     
     return (
       <div id="blog-post" className="wrapper">
@@ -18,11 +21,12 @@ export default class BlogPost extends Component {
         Still I'll rise.<br/>
         -Maya Angelou<br/>
 
-        {/* (remove the comment ticks and their surrounding brackets)
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        */}
+        {/* (remove the comment ticks and their surrounding brackets) */}
+        {/* <Comment commentText={comments[0]} />
+        <Comment commentText={comments[1]} />
+        <Comment commentText={comments[2]} /> */}
+        { comments.map(comment => <Comment commentText={ comment }/>)}
+       
       </div>
     );
   }
